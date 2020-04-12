d88b77ce6cb7ea667d83c4daffb146128888a8c8
Abhinav2907 <manu.mishra2907@gmail.com>
2020-04-12 17:19:40 +0530

    via upload

---
+++ burger-builder/src/components/Order/Order.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Order.css';
+const order = (props) => (
+    <div className={classes.Order}>
+        <p>Ingredients aaloo :1</p>
+        <p>Price: 90Rs</p>
+    </div>
+);
+
+export default order;
--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import {Route} from 'react-router-dom';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends React.Component {
     state = {
@@ -11,15 +13,22 @@ class Checkout extends React.Component {
             mayo: 1,
             aaloo: 1,
             meat: 0
-        }
+        },
+        totalPrice: 0
     }
-    componentDidMount () {
+    componentWillMount () {
         const query = new URLSearchParams(this.props.location.search);
         const ingredients ={};
+        let price = 0;
         for ( let param of query.entries()) {
-            ingredients[param[0]] = +param[1];
+            if(param[0]==='price') {
+                price = param[1];
+            }
+            else {
+                ingredients[param[0]] = +param[1];
+            }
         }
-        this.setState({ingredients: ingredients});
+        this.setState({ingredients: ingredients , totalPrice: price});
     }
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
@@ -35,6 +44,8 @@ class Checkout extends React.Component {
                     ingredients={this.state.ingredients}
                     checkoutContinued={this.checkoutContinuedHandler}
                     checkoutCancelled={this.checkoutCancelledHandler}/>
+                <Route path={this.props.match.path + '/contact-data'} 
+                    render={(props)=>(<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props}/>)} />
             </div>
         )
     }
